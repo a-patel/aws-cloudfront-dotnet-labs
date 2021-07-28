c66ef984e31e50087d500679cbaa2d275b797ca0
Ashish Patel <toaashishpatel@gmail.com>
2021-07-28 18:02:42 +0530

    Created service prototype and config options

---
+++ src/AWSCloudFrontLabs.SignedCookies.Api/Configuration/CloudFrontOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSCloudFrontLabs.SignedCookies.Api.Configuration
+{
+    public class CloudFrontOptions
+    {
+        public string CloudFrontUrl { get; set; }
+
+        public string KeyPairId { get; set; }
+
+        public string PrivateKey { get; set; }
+
+        public string ExpireDuration { get; set; }
+    }
+}
---
+++ src/AWSCloudFrontLabs.SignedCookies.Api/Services/AwsCloudFrontService.cs
@@ -0,0 +1,41 @@
+﻿#region Imports
+using Amazon.CloudFront;
+using AWSCloudFrontLabs.SignedCookies.Api.Configuration;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+#endregion
+
+namespace AWSCloudFrontLabs.SignedCookies.Api.Services
+{
+    public class AwsCloudFrontService
+    {
+        #region Members
+
+        private readonly CloudFrontOptions _cloudFrontOptions;
+        private readonly IAmazonCloudFront _cloudFrontClient;
+
+        #endregion
+
+        #region Ctor
+
+        public AwsCloudFrontService(IOptions<CloudFrontOptions> options)
+        {
+            _cloudFrontOptions = options.Value;
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public void GetSignedCookies()
+        {
+            var signer = _cloudFrontClient.(keyPairId, privateKey);
+        }
+
+        #endregion
+    }
+}
