33058955d95232c34f0126720d4ad9c73160b5fd
Ashish Patel <toaashishpatel@gmail.com>
2021-07-28 18:05:41 +0530

    added appsettings for CloudFront configuration

--- src/AWSCloudFrontLabs.SignedCookies.Api/Configuration/CloudFrontOptions.cs
+++ src/AWSCloudFrontLabs.SignedCookies.Api/Configuration/CloudFrontOptions.cs
@@ -7,12 +7,26 @@ namespace AWSCloudFrontLabs.SignedCookies.Api.Configuration
 {
     public class CloudFrontOptions
     {
-        public string CloudFrontUrl { get; set; }
+        /// <summary>
+        /// The URL or path for resource within a distribution.
+        /// </summary>
+        public string ResourceUrlOrPath { get; set; }
 
+        /// <summary>
+        /// The key pair id corresponding to the private key file given.
+        /// </summary>
         public string KeyPairId { get; set; }
 
-        public string PrivateKey { get; set; }
+        /// <summary>
+        /// Your private key file. RSA private key (.pem) are supported.
+        /// under `Configuration` folder
+        /// </summary>
+        public string PrivateKeyFileName { get; set; }
 
-        public string ExpireDuration { get; set; }
+        /// <summary>
+        /// The expiration duration till which content can be accessed using the generated cookies.
+        /// in seconds.
+        /// </summary>
+        public int ExpireDuration { get; set; }
     }
 }
--- src/AWSCloudFrontLabs.SignedCookies.Api/Startup.cs
+++ src/AWSCloudFrontLabs.SignedCookies.Api/Startup.cs
@@ -1,47 +1,44 @@
+using AWSCloudFrontLabs.SignedCookies.Api.Configuration;
+using AWSCloudFrontLabs.SignedCookies.Api.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AWSCloudFrontLabs.SignedCookies.Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CloudFrontOptions>(Configuration.GetSection("CloudFrontOptions"));
+            services.AddScoped<IAwsCloudFrontService, AwsCloudFrontService>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AWSCloudFrontLabs.SignedCookies.Api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AWS CloudFront Labs: SignedCookies Api", Version = "v1" });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AWSCloudFrontLabs.SignedCookies.Api v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AWS CloudFront Labs: SignedCookies Api v1"));
             }
 
             app.UseHttpsRedirection();
